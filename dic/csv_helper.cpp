5859299b48a8ca5756e1607139429edd4ac7ffa0
Olivier Teuliere <ipkiss@gmail.com>
2020-06-24 17:02:51 +0200

    Include <iostream> explicitly
    This is needed by recent versions of g++.

--- dic/csv_helper.cpp
+++ dic/csv_helper.cpp
@@ -18,6 +18,8 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  *****************************************************************************/
 
+#include <iostream>
+
 #include <boost/foreach.hpp>
 #include <boost/format.hpp>
 
