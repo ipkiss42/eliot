4ef180ce0c84d17c2c7143b581e734eda9e2d0e1
Olivier Teuliere <ipkiss@gmail.com>
2026-09-01 22:41:29 +0200

    Fix the ncurses build for windows

--- utils/curses_intf.h
+++ utils/curses_intf.h
@@ -23,16 +23,12 @@
 
 #include "config.h"
 
-#if defined HAVE_NCURSESW_CURSES_H
-#   include <ncursesw/curses.h>
+#if defined HAVE_NCURSESW_NCURSES_H
+#   include <ncursesw/ncurses.h>
 #elif defined HAVE_NCURSESW_H
 #   include <ncursesw.h>
-#elif defined HAVE_NCURSES_CURSES_H
-#   include <ncurses/curses.h>
 #elif defined HAVE_NCURSES_H
 #   include <ncurses.h>
-#elif defined HAVE_CURSES_H
-#   include <curses.h>
 #else
 //  Should not happen, if configure did its job correctly
 #   error "No header to include for ncursesw. Bad configure detection?"
